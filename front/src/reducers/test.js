31231fab11d3f70c8ec7605c252e31017c77ba4d
Thorron <thomas_caron@outlook.fr>
2020-07-24 16:45:18 +0200

    add index.js, test.js in src directory

---
+++ front/src/reducers/test.js
@@ -0,0 +1,12 @@
+//! == Import : local (actions)
+
+//! == Initial state
+export const initialState = {
+};
+
+//! == Actions to modified state
+const test = (state = initialState, action = {}) => {
+    return state;
+};
+
+export default test;
