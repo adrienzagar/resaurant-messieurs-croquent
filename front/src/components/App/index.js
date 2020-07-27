3230fafdef635770db4a49d3a5d6aec5652a246f
adrien zgr <adrien.zagar@gmail.com>
2020-07-27 13:25:03 +0200

    Footer is ok, not sure if responsive is useful for the moment

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -2,12 +2,7 @@
 import React from 'react';
 
 //! == Import : local
-<<<<<<< HEAD
 import Footer from '../Footer/index'
-=======
-import Footer from "../Footer/index";
->>>>>>> 019865b67d06899cb8be12ef21c398a59cae25f5
-import './styles.scss';
 
 function App() {
   return (
