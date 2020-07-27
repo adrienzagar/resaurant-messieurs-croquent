2f59b3e2add711685d93ac4c8a7b0407fdb04e3a
Thorron <thomas_caron@outlook.fr>
2020-07-27 22:32:26 +0200

    delete test in reducers index

--- front/src/reducers/index.js
+++ front/src/reducers/index.js
@@ -2,13 +2,10 @@
 import { combineReducers } from 'redux';
 
 //! == Import : local
-
-import test from './test';
 import header from './header';
 import footer from './footer';
 
 export default combineReducers({
-  test,
   header,
   footer,
 });
