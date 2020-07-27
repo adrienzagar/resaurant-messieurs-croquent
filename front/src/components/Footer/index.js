debbda6cc0257b87b6760e8d9606dc833c9c2577
adrien zgr <adrien.zagar@gmail.com>
2020-07-27 14:34:28 +0200

    add: logos clickable

--- front/src/components/Footer/index.js
+++ front/src/components/Footer/index.js
@@ -16,6 +16,20 @@ const Footer = () => (
                 <p className="footer__infos">25 Cours Pasteur,</p>
                 <p className="footer__infos">33000 BORDEAUX</p>
             </div>
+
+            <div className="footer__socialnetwork">
+            <ul>
+                <li>
+                    <a href="https://fr-fr.facebook.com/Messieurscroquent/" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook"></i></a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/messieurscroquent/" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
+                </li>
+                <li>
+                    <a href="https://www.tripadvisor.fr/Restaurant_Review-g187079-d12474378-Reviews-Messieurs_Croquent-Bordeaux_Gironde_Nouvelle_Aquitaine.html" target="_blank" rel="noopener noreferrer"><i className="fab fa-tripadvisor"></i></a>
+                </li>
+             </ul>
+            </div>
         </div>
   </footer>
 );
