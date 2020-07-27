bf365102bfc16146be90494b2bd5898aca36b67a
imed <tousrenov2@gmail.com>
2020-07-27 23:46:56 +0200

    add a components card

--- front/src/index.js
+++ front/src/index.js
@@ -1,4 +1,7 @@
 //! == Import : npm
+import '@fortawesome/fontawesome-free/css/all.min.css'; 
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
