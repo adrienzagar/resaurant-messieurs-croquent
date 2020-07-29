713f32610addb23baf7a3a812b5ee975367118ca
adrienzagar <etudiant@teleporteur.oclock>
2020-07-29 12:18:36 +0200

    fix: cleaning master

--- front/src/index.js
+++ front/src/index.js
@@ -1,7 +1,7 @@
 //! == Import : npm
-import '@fortawesome/fontawesome-free/css/all.min.css'; 
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
+//import '@fortawesome/fontawesome-free/css/all.min.css'; 
+//import 'bootstrap-css-only/css/bootstrap.min.css';
+//import 'mdbreact/dist/css/mdb.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
