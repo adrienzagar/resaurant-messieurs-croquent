94e8309f387abdd60d2f34d71ec79853aa68f816
Thorron <thomas_caron@outlook.fr>
2020-07-29 20:05:00 +0200

    add component carousel and add image for carousel in directory assets

--- front/src/components/Home/HomeCarousel/index.js
+++ front/src/components/Home/HomeCarousel/index.js
@@ -1,13 +1,28 @@
 //! == Import : npm ==
 import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
 // import PropTypes from 'prop-types';
 
 //! == Import : local ==
+import carousel1 from '../../../assets/images/carousel/carousel1.jpg';
+import carousel2 from '../../../assets/images/carousel/carousel2.jpg';
+import carousel3 from '../../../assets/images/carousel/carousel3.jpg';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; 
 import './styles.scss';
 
 //! == Composant ==
 const HomeCarousel = () => (
-    <div>HomeCarousel</div>
+    <Carousel infiniteLoop stopOnHover={false} autoPlay className="carousel">
+        <div className="carousel__container">
+            <img className="carousel__image" src={carousel1} alt="restaurant"/>
+        </div>
+        <div className="carousel__container">
+            <img className="carousel__image" src={carousel2} alt="restaurant"/>
+        </div>
+        <div className="carousel__container">
+            <img className="carousel__image" src={carousel3} alt="restaurant"/>
+        </div>
+    </Carousel>
 );
 
 // HomeCarousel.propTypes = {};
