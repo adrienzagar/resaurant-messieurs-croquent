74e49522239f6ea16075e85afa54a4f244a4b82c
adrienzagar <etudiant@teleporteur.oclock>
2020-07-29 15:53:37 +0200

    style: Integration complete, still needs some little fix

--- front/src/components/Command/Product/index.js
+++ front/src/components/Command/Product/index.js
@@ -7,20 +7,52 @@ import './styles.scss';
 
 //! == Composant ==
 const Product = ({ products }) => {
-    console.log(products);
+    // console.log(products);
     return (
         <div className="product">
-            {products.map((product) => (
-                <ul key={product.id}>
-                    <li>{product.name}</li>
-                    <li>{product.description}</li>
-                    <li>{product.price}€</li>
-                    <li></li>
-                </ul>
-            ))}
+            <h1 className="product__category">Croque monsieur</h1>
+            <div className="product__container">
+                <img className="product__image" src="https://i.ibb.co/xDQ8NNP/Coq.jpg"></img>
+                <p className="product__dish">Le transformiste</p>
+                <p className="product__description">Jambon blanc, béchamel maison, fromage Abondance A.O.P,\r\noeuf fermier</p>
+                <p className="product__price">8€</p>
+                <button className="product__add">
+                <i class="fa fa-plus" aria-hidden="true"></i>
+                </button>
+            </div>
+            <div className="product__container">
+                <img className="product__image" src="https://i.ibb.co/xDQ8NNP/Coq.jpg"></img>
+                <p className="product__dish">Le transformiste</p>
+                <p className="product__description">Jambon blanc, béchamel maison, fromage Abondance A.O.P,\r\noeuf fermier</p>
+                <p className="product__price">8€</p>
+                <button className="product__add">
+                <i class="fa fa-plus" aria-hidden="true"></i>
+                </button>
+            </div>
+            <div className="product__container">
+                <img className="product__image" src="https://i.ibb.co/xDQ8NNP/Coq.jpg"></img>
+                <p className="product__dish">Le transformiste</p>
+                <p className="product__description">Jambon blanc, béchamel maison, fromage Abondance A.O.P,\r\noeuf fermier</p>
+                <p className="product__price">8€</p>
+                <button className="product__add">
+                <i class="fa fa-plus" aria-hidden="true"></i>
+                </button>
+            </div>
+
         </div>
     );
 };
+
+// {products.map((product) => (
+//     <ul key={product.id} className="product__dishlist">
+//         <li className="product__dish">
+//             <p>{product.category.name}</p>  
+//             <p>{product.name}</p>  
+//             <p>{product.description}</p>  
+//             <p>{product.price}</p>  
+//         </li>
+//     </ul>
+// ))}
 
 // Product.propTypes = {};
 
