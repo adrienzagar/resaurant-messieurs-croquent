7adae283e00e123d8f87c4b8838bb968cc89a4e3
adrienzagar <etudiant@teleporteur.oclock>
2020-07-29 09:35:20 +0200

    fix: git revert commit 30e609ffdb928351c3dde3423bda231b330db3aa

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -2,28 +2,15 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-<<<<<<< HEAD
-=======
 //! == Import : local
 import Header from '../../containers/Header';
 import Command from '../../containers/Command';
 import Footer from '../../containers/Footer';
->>>>>>> master
 import './styles.scss';
 import FindUs from '../findUS';
 
 
 
-<<<<<<< HEAD
-function App() {
-  return (
-    <div>
-    <FindUs />
- 
-    </div>
-  );
-}
-=======
 const App = () => (
   <div className="app">
     <Header />
@@ -33,6 +20,5 @@ const App = () => (
     <Footer />
   </div>
 );
->>>>>>> master
 
 export default App;
