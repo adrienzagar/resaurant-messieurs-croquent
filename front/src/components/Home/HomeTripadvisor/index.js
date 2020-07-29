5f0baa729d10b10cb8ad6316fec0be72310f4999
Thorron <thomas_caron@outlook.fr>
2020-07-29 23:43:58 +0200

    add component Tripadvisor, with react-helmet  dependence

--- front/src/components/Home/HomeTripadvisor/index.js
+++ front/src/components/Home/HomeTripadvisor/index.js
@@ -1,16 +1,26 @@
 //! == Import : npm ==
 import React from 'react';
-// import PropTypes from 'prop-types';
+import AOS from 'aos';
 
 //! == Import : local ==
+import TripadvisorMax from './TripadvisorMax';
+import TripadvisorMin from './TripadvisorMin';
+import 'aos/dist/aos.css';
 import './styles.scss';
 
 //! == Composant ==
-const HomeTripadvisor = () => (
-    <div>HomeTripadvisor</div>
-);
-
-// HomeTripadvisor.propTypes = {};
+const HomeTripadvisor = () => {
+    AOS.init();
+    return (
+        <div className="homeTripadvisor">
+            <h2 className="homeTripadvisor__title" data-aos="fade-up">
+                Parce que votre avis compte
+            </h2>
+            <TripadvisorMax />
+            <TripadvisorMin />
+        </div>
+    );
+};
 
 //! == Export ==
 export default HomeTripadvisor;
