b30bd77bcb70edb1c48be3f152306b656eb83e3f
Thorron <thomas_caron@outlook.fr>
2020-07-30 18:29:47 +0200

    create directory Err404

---
+++ front/src/components/Err404/index.js
@@ -0,0 +1,25 @@
+//! == Import : npm ==
+import React from 'react';
+
+//! == Import : local ==
+import './styles.scss';
+
+//! == Composant ==
+const Err404 = () => (
+    <main className="error">
+        <h1 className="error__title">
+            <span className="error__4">
+                4
+            </span>
+            <span className="error__0">
+                0
+            </span>
+            <span className="error__4">
+                4
+            </span>
+        </h1>
+    </main>
+);
+
+//! == Export ==
+export default Err404;
