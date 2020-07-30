30448a081bb69544a29978055d4356250619c591
Thorron <thomas_caron@outlook.fr>
2020-07-31 00:18:26 +0200

    add style and path for page 404

--- front/src/components/Err404/index.js
+++ front/src/components/Err404/index.js
@@ -1,25 +1,33 @@
 //! == Import : npm ==
 import React from 'react';
+import AOS from 'aos';
 
 //! == Import : local ==
+import 'aos/dist/aos.css';
 import './styles.scss';
 
 //! == Composant ==
-const Err404 = () => (
-    <main className="error">
-        <h1 className="error__title">
-            <span className="error__4">
-                4
-            </span>
-            <span className="error__0">
-                0
-            </span>
-            <span className="error__4">
-                4
-            </span>
-        </h1>
-    </main>
-);
+const Err404 = () => {
+    AOS.init();
+    return (
+        <main className="error">
+            <h1 className="error__title">
+                <span className="error__start" data-aos="fade-right">
+                    4
+                </span>
+                <span className="error__center" data-aos="fade-down">
+                    0
+                </span>
+                <span className="error__end" data-aos="fade-left">
+                    4
+                </span>
+            </h1>
+            <p className="error__text" data-aos="fade-up">
+                La page que vous cherchez n'existe pas
+            </p>
+        </main>
+    );
+};
 
 //! == Export ==
 export default Err404;
