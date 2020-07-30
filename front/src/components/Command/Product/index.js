bb5df592a69f192219cb1f9b5a05511b453b9413
adrienzagar <etudiant@teleporteur.oclock>
2020-07-30 11:43:06 +0200

    feat: diplaying products for each categories

--- front/src/components/Command/Product/index.js
+++ front/src/components/Command/Product/index.js
@@ -3,47 +3,55 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 
 //! == Import : local ==
-import Title from './'
 import './styles.scss';
 
 //! == Composant ==
 const Product = ({ products, categories }) => {
-    console.log(products);
-    console.log(categories);
+    // console.log(products);
+    // console.log(categories);
+
     return (
         <div className="product">
-            {categories.map((category) => (
-                
-                <h1 key={category.id} className="product__category">{category.name}</h1>
-            ))}
-            {products.map((product) => (
-            
-            <div className="product__container" key={product.id}>
-                <img className="product__image" src="https://i.ibb.co/xDQ8NNP/Coq.jpg"></img>
-                <p className="product__dish">{product.name}</p>
-                <p className="product__description">{product.description}</p>
-                <p className="product__price">{product.price}€</p>
-                <button className="product__add">
-                    <i className="fa fa-plus" aria-hidden="true"></i>
-                </button>
-            </div>
-            ))}
+                {categories.map((category) => {
+                    const result = products.filter(product => product.category.id === category.id);
+                        console.log(result)
+                    return (
+                        <div>
+                            <h1 key={category.id} className="product__category">{category.name}</h1>
+                            {result.map((productMap) => (
+                                <div className="product__container" key={productMap.id}>
+                                    <img className="product__image" src="https://i.ibb.co/xDQ8NNP/Coq.jpg"></img>
+                                    <p className="product__dish">{productMap.name}</p>
+                                    <p className="product__description">{productMap.description}</p>
+                                    <p className="product__price">{productMap.price}€</p>
+                                    <button className="product__add">
+                                        <i className="fa fa-plus" aria-hidden="true"></i>
+                                    </button>
+                                </div> 
+                            ))}
+                        </div>
+                    )
+                })}
         </div>
     );
 };
 
-// {products.map((product) => (
-//     <ul key={product.id} className="product__dishlist">
-//         <li className="product__dish">
-//             <p>{product.category.name}</p>  
-//             <p>{product.name}</p>  
-//             <p>{product.description}</p>  
-//             <p>{product.price}</p>  
-//         </li>
-//     </ul>
+// {categories.map((category) => (
+                
+//     <h1 key={category.id} className="product__category">{category.name}</h1>
 // ))}
+// {products.map((product) => (
 
-// Product.propTypes = {};
+// <div className="product__container" key={product.id}>
+//     <img className="product__image" src="https://i.ibb.co/xDQ8NNP/Coq.jpg"></img>
+//     <p className="product__dish">{product.name}</p>
+//     <p className="product__description">{product.description}</p>
+//     <p className="product__price">{product.price}€</p>
+//     <button className="product__add">
+//         <i className="fa fa-plus" aria-hidden="true"></i>
+//     </button>
+// </div>
+// ))}
 
 //! == Export ==
 export default Product;
