531e31e8cccd3fc5b3b070cb4df672c990f105f8
adrienzagar <etudiant@teleporteur.oclock>
2020-07-31 15:05:08 +0200

    modifying cart display

--- front/src/components/Command/Cart/index.js
+++ front/src/components/Command/Cart/index.js
@@ -6,25 +6,31 @@ import './styles.scss';
 const Cart = ({ cart, quantity }) => {
     console.log(cart, "Panier de produit");
     console.log(quantity, "quantité");
+    {cart.map((product) => ( console.log(product.name, "TEST")))}
     return(
+
   <div className="cart">
-    <div className="cart__container">
-        <div className="cart__dishes">
-            <h1 className="cart__dish"></h1>
-            <img className="cart__image" src="https://i.ibb.co/xDQ8NNP/Coq.jpg" alt=""/>
+    {cart.map((product) => (
+        <div className="cart__container">
+            <div className="cart__dishes">
+                <h1 className="cart__dish">{product.name}</h1>
+            </div>
+            <div className="cart__prices">
+                <p>{product.price}&euro;</p>
+            </div>
+            <div className="cart__quantity">
+                <button className="cart__remove">
+                    <i className="fa fa-minus" aria-hidden="true" ></i>
+                </button>
+                <input className="cart__count" type="text" />
+                <button 
+                    className="cart__add"
+                >
+                    <i className="fa fa-plus" aria-hidden="true"></i>
+                </button>
+            </div>
         </div>
-        <div className="cart__quantity">
-            <button className="cart__remove">
-                <i className="fa fa-minus" aria-hidden="true" ></i>
-            </button>
-            <input className="cart__count" type="text" placeholder={quantity}/>
-            <button 
-                className="cart__add"
-            >
-                <i className="fa fa-plus" aria-hidden="true"></i>
-            </button>
-        </div>
-    </div>
+    ))}
     <div className="cart__total">
         <h1>TOTAL</h1>
         <p>22€</p>
--- front/src/components/Command/index.js
+++ front/src/components/Command/index.js
@@ -36,7 +36,7 @@ const Command = ({
              />
              <Cart 
                 cart={cart} 
-                quantity={quantity}    
+                quantity={quantity}
              />
         </main>
 );}
