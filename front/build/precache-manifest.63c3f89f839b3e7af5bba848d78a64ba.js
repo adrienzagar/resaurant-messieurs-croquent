3b9bdd118f1dd28c9a740626e2081ab80752e99a
adrienzagar <etudiant@teleporteur.oclock>
2020-08-01 22:57:18 +0200

    build test

---
+++ front/build/precache-manifest.63c3f89f839b3e7af5bba848d78a64ba.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "261e824da1ad4faa2fcdc9de947e7110",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cfcad1a7ffd3729054b",
+    "url": "/static/css/2.bca29b35.chunk.css"
+  },
+  {
+    "revision": "2840f2bff8dd7489c5d1",
+    "url": "/static/css/main.728858fd.chunk.css"
+  },
+  {
+    "revision": "0cfcad1a7ffd3729054b",
+    "url": "/static/js/2.fb0698c8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.fb0698c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2840f2bff8dd7489c5d1",
+    "url": "/static/js/main.d4aeaf77.chunk.js"
+  },
+  {
+    "revision": "23f7e4d2c8397ce55bf6",
+    "url": "/static/js/runtime-main.5fe610d3.js"
+  },
+  {
+    "revision": "0513c2ecf470684dd041f7bd2638ce61",
+    "url": "/static/media/carousel1.0513c2ec.jpg"
+  },
+  {
+    "revision": "c0e2641dac3f2dedae4e423516215b4a",
+    "url": "/static/media/carousel2.c0e2641d.jpg"
+  },
+  {
+    "revision": "a9e84b078ae47d5614ea2c37d5cd5ab2",
+    "url": "/static/media/carousel3.a9e84b07.jpg"
+  },
+  {
+    "revision": "f8d6ce728e9d74dbf94e9c8ccf11e2e2",
+    "url": "/static/media/logo-brand.f8d6ce72.png"
+  }
+]);
