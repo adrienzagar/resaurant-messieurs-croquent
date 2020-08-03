4cce7ed3058e023ffadb204b2f8ed1a2b80c5a28
Imed <49333128+Imed27@users.noreply.github.com>
2020-08-03 22:52:13 +0200

    creation of a counter for adding and removing products from the cart

--- front/src/reducers/command.js
+++ front/src/reducers/command.js
@@ -1,93 +1,138 @@
 //! == Import : local (actions)
-import { ADD_PRODUCT_TO_CART, SAVE_PRODUCT, ADD_QUANTITY_PRODUCT, SAVE_PRICE } from '../actions/product';
+import { ADD_PRODUCT_TO_CART, SAVE_PRODUCT, ADD_QUANTITY_PRODUCT, REMOVE_QUANTITY_PRODUCT, SAVE_PRICE } from '../actions/product';
 import { SAVE_CATEGORIES } from '../actions/categories';
 import { SET_MAIL_VALUE, SET_PHONE_VALUE, SET_NAME_VALUE, SET_LASTNAME_VALUE } from '../actions/form';
+import { waitForDomChange } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 
 //! == Initial state
 export const initialState = {
-    links: [
-        {
-           id: 1,
-           name: 'ubereats',
-           path: 'https://www.ubereats.com/fr/bordeaux/food-delivery/messieurs-croquent/zZEABPXlT7amSdTvZnDiAw?ps=1',
-           src: 'https://i2.wp.com/www.adriasbakery.com/wp-content/uploads/2019/01/uber-eats-logo-png.png',
-           fade: 'fade-right'
-        },
-        {
-            id: 2,
-            name: 'deliveroo',
-            path: 'https://deliveroo.fr/fr/menu/bordeaux/bordeaux-saint-genes-victoire/messieurs-croquent',
-            src: 'https://img.over-blog-kiwi.com/0/55/35/69/20200209/ob_84bc94_deliveroo-logo-svg.png',
-            fade: 'fade-left'
-         },
-    ],
-    listProduct: [],
-    listCategories: [],
-    listPrice: [],
-    quantity: 0,
-    quantities: 0,
-    cart: [],
-    mailValue: '',
-    phoneValue: '',
-    nameValue: '',
-    lastnameValue: '',
+  links: [
+    {
+      id: 1,
+      name: 'ubereats',
+      path: 'https://www.ubereats.com/fr/bordeaux/food-delivery/messieurs-croquent/zZEABPXlT7amSdTvZnDiAw?ps=1',
+      src: 'https://i2.wp.com/www.adriasbakery.com/wp-content/uploads/2019/01/uber-eats-logo-png.png',
+      fade: 'fade-right'
+    },
+    {
+      id: 2,
+      name: 'deliveroo',
+      path: 'https://deliveroo.fr/fr/menu/bordeaux/bordeaux-saint-genes-victoire/messieurs-croquent',
+      src: 'https://img.over-blog-kiwi.com/0/55/35/69/20200209/ob_84bc94_deliveroo-logo-svg.png',
+      fade: 'fade-left'
+    },
+  ],
+  listProduct: [],
+  listCategories: [],
+  listPrice: [],
+  quantity: 0,
+  quantities: 0,
+  cart: [],
+  mailValue: '',
+  phoneValue: '',
+  nameValue: '',
+  lastnameValue: '',
 };
 
 //! == Actions to modified state
 const command = (state = initialState, action = {}) => {
-    switch (action.type) {
-        case SAVE_PRODUCT:
-          return {
-            ...state,
-            listProduct: action.listProduct,
-          };
-        case SAVE_CATEGORIES:
-          return {
-            ...state,
-            listCategories: action.categories,
-          };
-          case ADD_PRODUCT_TO_CART:
-            state.cart.push(action.product)
-            return {
-              ...state,
-              quantity: state.quantity + 1,
-              cart: state.cart,
-            };
-          case ADD_QUANTITY_PRODUCT:
-            return {
-              ...state,
-              quantities: state.quantities +1
-            }
-            case SAVE_PRICE:
-            state.listPrice.push(action.price)
-            return {
-              ...state,
-              listPrice: state.listPrice,
-            };
-            case SET_MAIL_VALUE:
-              return {
-                ...state,
-                mailValue: action.mailValue,
-              };
-            case SET_PHONE_VALUE:
-              return {
-                ...state,
-                phoneValue: action.phoneValue,
-              };
-            case SET_NAME_VALUE:
-              return {
-                ...state,
-                nameValue: action.nameValue,
-              };
-            case SET_LASTNAME_VALUE:
-              return {
-                ...state,
-                lastnameValue: action.lastnameValue,
-              };
-        default:
-            return state;
-    }
-    
+  var uniqueProds = [];
+
+  switch (action.type) {
+    case SAVE_PRODUCT:
+      return {
+        ...state,
+        listProduct: action.listProduct,
+      };
+    case SAVE_CATEGORIES:
+      return {
+        ...state,
+        listCategories: action.categories,
+      };
+    case ADD_PRODUCT_TO_CART:
+      state.cart.push(action.product)
+      state.cart = state.cart.map(product => {
+        if (product.id === action.product.id) {
+          return { ...product, quantity: product.quantity == null ? 1 : product.quantity + 1 }
+        }
+        else {
+          return product
+        }
+      })
+      state.cart.map(prod => {
+        console.log(prod.id)
+        console.log(uniqueProds.includes(prod.id))
+        if (!uniqueProds.includes(prod.id)) {
+          uniqueProds.push(prod)
+        }
+        return uniqueProds
+      }
+      )
+      console.log("me", uniqueProds)
+      return {
+        ...state,
+        quantity: state.quantity + 1,
+        cart: state.cart
+      };
+    case ADD_QUANTITY_PRODUCT:
+      console.log(action.quantity)
+      state.cart = state.cart.map(product => {
+        if (product.id === action.quantity.id) {
+          state.listPrice.push(action.quantity.price)
+          return { ...product, quantity: product.quantity + 1 }
+        }
+        return product
+      })
+      return {
+        ...state,
+        cart: state.cart
+      };
+    case REMOVE_QUANTITY_PRODUCT:
+      console.log(action.quantity)
+      state.cart = state.cart.map(product => {
+        if (product.id === action.quantity.id) {
+          state.listPrice.pop(action.quantity.price)
+          if (product.quantity > 0){
+            return { ...product, quantity: product.quantity - 1 }
+          }
+        }
+        return product
+      })
+      return {
+        ...state,
+        quantities: state.quantities -= 1
+      };
+    case SAVE_PRICE:
+      state.listPrice.push(action.price)
+      return {
+        ...state,
+        listPrice: state.listPrice,
+      };
+    case SET_MAIL_VALUE:
+      return {
+        ...state,
+        mailValue: action.mailValue,
+      };
+    case SET_PHONE_VALUE:
+      return {
+        ...state,
+        phoneValue: action.phoneValue,
+      };
+    case SET_NAME_VALUE:
+      return {
+        ...state,
+        nameValue: action.nameValue,
+      };
+    case SET_LASTNAME_VALUE:
+      return {
+        ...state,
+        lastnameValue: action.lastnameValue,
+      };
+    default:
+      return state;
+  }
+
 };
 
 export default command;
