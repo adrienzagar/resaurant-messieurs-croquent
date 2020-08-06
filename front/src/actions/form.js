ab052ed567a8769d130d40a67f2eb9fe22dfce55
adrienzagar <adrien.zagar@gmail.com>
2020-08-06 16:41:12 +0200

    add comment in the order form

--- front/src/actions/form.js
+++ front/src/actions/form.js
@@ -2,7 +2,8 @@ export const SET_MAIL_VALUE = 'SET_MAIL_VALUE';
 export const SET_PHONE_VALUE = 'SET_PHONE_VALUE';
 export const SET_NAME_VALUE = 'SET_NAME_VALUE';
 export const SET_LASTNAME_VALUE = 'SET_LASTNAME_VALUE';
-export const SET_USER_OBJECT= 'SET_USER_OBJECT'
+export const SET_USER_OBJECT = 'SET_USER_OBJECT';
+export const SET_COMMENT_VALUE = 'SET_COMMENT_VALUE'
 
 // ACTION CREATOR
 export const setMailValue = (email) => ({
@@ -24,6 +25,11 @@ export const setLastnameValue = (lastname) => ({
     type: SET_LASTNAME_VALUE,
     lastname
 });
+
+export const setCommentValue= (comment) => ({
+    type: SET_COMMENT_VALUE,
+    comment
+})
 
 export const setUserObject = (lastname, firstname, phone_number, email) => ({
     type: SET_USER_OBJECT,
