2303d097667eb0651b4a6f1d04f60b4f12a341e4
adrien zgr <adrien.zagar@gmail.com>
2020-08-13 09:29:22 +0200

    deployment back and front

---
+++ front/build/precache-manifest.946ca761ea39e6ba8549fd1b6bb1cbf6.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6759cc3e3185408b17c7d9f39ae72a27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9edf4095a351db8b970",
+    "url": "/static/css/2.00f16649.chunk.css"
+  },
+  {
+    "revision": "dcea25a9790a81406f8f",
+    "url": "/static/css/main.25e24816.chunk.css"
+  },
+  {
+    "revision": "c9edf4095a351db8b970",
+    "url": "/static/js/2.5a7e8118.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.5a7e8118.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcea25a9790a81406f8f",
+    "url": "/static/js/main.8657f0b5.chunk.js"
+  },
+  {
+    "revision": "23f7e4d2c8397ce55bf6",
+    "url": "/static/js/runtime-main.5fe610d3.js"
+  },
+  {
+    "revision": "44d00078791b1cb2a8fcf8628547b6f1",
+    "url": "/static/media/background-validate.44d00078.jpg"
+  },
+  {
+    "revision": "dbdaab2c06e9546d411fb0d0ed6afe7b",
+    "url": "/static/media/beer.dbdaab2c.jpg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "588c796629f90f31080e3cb74647f920",
+    "url": "/static/media/cake.588c7966.jpg"
+  },
+  {
+    "revision": "6a8338ebd2c3320cc4a60a5e2b8f9b8b",
+    "url": "/static/media/carousel1.6a8338eb.jpg"
+  },
+  {
+    "revision": "c1a8d0114c0b906bdee5958445d36fca",
+    "url": "/static/media/carousel2.c1a8d011.jpg"
+  },
+  {
+    "revision": "0509c7c5ef1f810913e50a34aff2f5f8",
+    "url": "/static/media/carousel3.0509c7c5.jpg"
+  },
+  {
+    "revision": "4a8c495bbdebd570301ae94190c87e4d",
+    "url": "/static/media/chips.4a8c495b.jpg"
+  },
+  {
+    "revision": "f9d433d70057a3a15bb4886d1530433b",
+    "url": "/static/media/coleslaw.f9d433d7.jpg"
+  },
+  {
+    "revision": "3645444f31c3bcceb078fb2581de4aa6",
+    "url": "/static/media/confit.3645444f.jpg"
+  },
+  {
+    "revision": "ea7e1465a601a0e99efd02eddd432086",
+    "url": "/static/media/coq.ea7e1465.jpg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "f8d6ce728e9d74dbf94e9c8ccf11e2e2",
+    "url": "/static/media/logo-brand.f8d6ce72.png"
+  },
+  {
+    "revision": "4a091de90e7d2399e9665f6b780521bf",
+    "url": "/static/media/moment.4a091de9.jpg"
+  },
+  {
+    "revision": "19ce9a77f33a2f877dda9b2e74bbf740",
+    "url": "/static/media/mrcroque.19ce9a77.jpg"
+  },
+  {
+    "revision": "70c730115fc3a9207752c2d53a0523f9",
+    "url": "/static/media/mystere.70c73011.jpg"
+  },
+  {
+    "revision": "0a0a70b479d861dade81747cfcaabe49",
+    "url": "/static/media/nugget.0a0a70b4.jpg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "c2d1fd9621e98cf09f91203f6f79049e",
+    "url": "/static/media/planche.c2d1fd96.jpg"
+  },
+  {
+    "revision": "ce6e78add3783da1f84d4e1ab9698bba",
+    "url": "/static/media/salad.ce6e78ad.jpg"
+  },
+  {
+    "revision": "c99cb05c858eb846db9044e3867c765f",
+    "url": "/static/media/seguin.c99cb05c.jpg"
+  },
+  {
+    "revision": "817dd2a49e5119eb88d3e5dd87fe63c5",
+    "url": "/static/media/soda.817dd2a4.jpg"
+  },
+  {
+    "revision": "01c873c1613d67714abf5c6a796100a3",
+    "url": "/static/media/transformiste.01c873c1.jpg"
+  },
+  {
+    "revision": "f2bc3918782e477eb076433a128ebc55",
+    "url": "/static/media/vegetable.f2bc3918.jpg"
+  }
+]);
