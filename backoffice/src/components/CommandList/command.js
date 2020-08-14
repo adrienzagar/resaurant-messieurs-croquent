abc73960174647ffb2c71ff54c806e110e1037ec
Thorron <thomas_caron@outlook.fr>
2020-08-14 16:09:10 +0200

    archive command on click in BDD

--- backoffice/src/components/CommandList/command.js
+++ backoffice/src/components/CommandList/command.js
@@ -5,30 +5,45 @@ import React, { useEffect } from 'react';
 import './styles.scss';
 
 //! == Composant ==
-const Command = ({ getCommand, commands }) => {
+const Command = ({ getCommand, commands, changeStatus, saveId }) => {
     useEffect(() => { getCommand(); }, []);
+    const handleArchivate = (command) => {
+        saveId(command.id);
+        changeStatus();
+    }
     return (
         <div className="command">
-            {commands.map((command) => (
-                <div className="command__container">
-                    <p className="command__number">Commande n°{command.id}</p>
-                    <p className="command__infosUser">Informations du client :</p>
-                    <div className="command__user">
-                        <p className="command__infos">Prénom: {command.user.firstname}</p>
-                        <p className="command__infos">Nom: {command.user.lastname}</p>
-                        <p className="command__infos">Email: {command.user.email}</p>
-                        <p className="command__infos">Téléphone: 0{command.user.phoneNumber}</p>
-                    </div>
-                    <p className="command__infosCommand">Détail de la commande :</p>
-                    {command.orderLines.map((order) => (
-                        <p className="command__detail">{order.quantity} {order.product.name}</p>
-                    ))}
-                    <p className="command__infosComment">Commentaire :</p>
-                    <p className="command__comment">{command.comment}</p>
-                    <p className="command__infosPrice">Prix total de la commande :</p>
-                    <p className="command__price">{command.price} &euro;</p>
-                </div>
-            ))}
+            {commands.map((command) => {
+                if (command.status === 1) {
+                    return (
+                        <div className="command__container">
+                            <p className="command__number">Commande n°{command.id}</p>
+                            <p className="command__infosUser">Informations du client :</p>
+                            <div className="command__user">
+                                <p className="command__infos">Prénom: {command.user.firstname}</p>
+                                <p className="command__infos">Nom: {command.user.lastname}</p>
+                                <p className="command__infos">Email: {command.user.email}</p>
+                                <p className="command__infos">Téléphone: 0{command.user.phoneNumber}</p>
+                            </div>
+                            <p className="command__infosCommand">Détail de la commande :</p>
+                            {command.orderLines.map((order) => (
+                                <p className="command__detail">{order.quantity} {order.product.name}</p>
+                            ))}
+                            <p className="command__infosComment">Commentaire :</p>
+                            <p className="command__comment">{command.comment}</p>
+                            <p className="command__infosPrice">Prix total de la commande :</p>
+                            <p className="command__price">{command.price} &euro;</p>
+                            <button 
+                                className="command__archivate"
+                                onClick={() => (handleArchivate(command))}
+                            >
+                                <i class="fas fa-archive"></i>
+                                Archiver
+                            </button>
+                        </div>
+                    )
+                };
+            })}
         </div>
     );
 };
