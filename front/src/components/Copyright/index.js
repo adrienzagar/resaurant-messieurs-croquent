aa1a9a29096776086e897729d4dfad7512d5919d
adrien zgr <adrien.zagar@gmail.com>
2020-08-17 11:55:23 +0200

    little fix for deployement

--- front/src/components/Copyright/index.js
+++ front/src/components/Copyright/index.js
@@ -10,11 +10,11 @@ const Copyright = () => {
     useEffect (() => { document.title = "Messieurs Croquent - Copyright" }, []);
     return(
     <div className="card">
-        <Card picture="https://ca.slack-edge.com/TTY8DUCTZ-UUVS83BM3-4dc5e39d9bc5-512" name="Johan" post="Product Owner & Developer Back-End" />
+        <Card picture="https://ca.slack-edge.com/TTY8DUCTZ-UUVS83BM3-4dc5e39d9bc5-512" name="Johan" post="Developer Back-End & Product Owner" />
         <Card picture="https://ca.slack-edge.com/TTY8DUCTZ-U0151S58ZS9-7dce0850c70b-512" name="Mohamed" post="Lead Developer Back-End" />
-        <Card picture="https://ca.slack-edge.com/TTY8DUCTZ-UUFG6K9RV-e5f6a25b4c5d-512" name="Imed" post="Git Master & Developer Front-End" />
-        <Card picture="https://ca.slack-edge.com/TTY8DUCTZ-UUBMYJBL1-6cc5911edd1e-512" name="Thomas" post="Lead Developer Front-End & Integrator" />
-        <Card picture="https://ca.slack-edge.com/TBGQ2SW6T-UUK236ZJA-4bae652bc792-192" name="Adrien" post="Scrum Master & Developer Front-End" />
+        <Card picture="https://ca.slack-edge.com/TTY8DUCTZ-UUFG6K9RV-aea5c668b138-512" name="Imed" post="Developer Front-End & Git Master" />
+        <Card picture="https://media-exp1.licdn.com/dms/image/C5603AQHj89Xuo8ZiJw/profile-displayphoto-shrink_200_200/0?e=1603324800&v=beta&t=uXDIsEMWTwVzq0IYCAq06P7fbWF4jJeXsVDXjRPGtGg" name="Thomas" post="Lead Developer Front-End & Integrator" />
+        <Card picture="https://ca.slack-edge.com/TBGQ2SW6T-UUK236ZJA-4bae652bc792-192" name="Adrien" post="Developer Front-End & Scrum Master" />
     </div>
 )};
 
--- front/src/components/Home/HomeTripadvisor/index.js
+++ front/src/components/Home/HomeTripadvisor/index.js
@@ -12,8 +12,6 @@ import './styles.scss';
 const HomeTripadvisor = () => {
     AOS.init();
 
-    const find = Object.keys(window)
-    console.log(find)
     return (
         <div className="homeTripadvisor">
             <h2 className="homeTripadvisor__title" data-aos="fade-up">
